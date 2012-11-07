4419e4fc2238e8ac38e6f0bcc464f1613359e1bd
翟 晓宇 <zhaixiaoyu@145.39.167.220.broad.cd.sc.dynamic.163data.com.cn>
2012-11-07 21:26:32 +0800

    Initial Commit

---
+++ dot&dash/RYTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RYTAppDelegate.h
+//  dot&dash
+//
+//  Created by 翟 晓宇 on 12-11-7.
+//  Copyright (c) 2012年 Ryantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RYTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ dot&dash/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  dot&dash
+//
+//  Created by 翟 晓宇 on 12-11-7.
+//  Copyright (c) 2012年 Ryantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RYTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RYTAppDelegate class]));
+    }
+}
